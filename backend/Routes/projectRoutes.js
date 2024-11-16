89a2a11e355ffc0191b2f162da266d7051d8fa8f
Akshat Saxena <128921738+GitHub-Akshat@users.noreply.github.com>
2024-11-16 21:05:00 +0530

    Update projectRoutes.js

--- backend/Routes/projectRoutes.js
+++ backend/Routes/projectRoutes.js
@@ -5,9 +5,11 @@ const projectRoutes = express.Router();
 import upload from "../Middleware/multer.js";
 import { AddProject, DeleteProject, GetProjects } from "../controller/project.js";
 
+
 projectRoutes.get("/check-auth" , verifyToken, checkAuth);
 projectRoutes.get("/getprojects", GetProjects);
 projectRoutes.post("/addprojects", verifyToken, upload.single('image'), AddProject);
 projectRoutes.delete("/deleteproject/:id", verifyToken, DeleteProject); 
+
 
 export default projectRoutes
